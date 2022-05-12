b48201509cb3ba31cc2b035d56b38c15e7b4380b
0xLeif <zmeriksen@icloud.com>
2022-05-11 18:11:27 -0600

    Initial Commit

---
+++ ComposableCacheStoreExample/ComposableCacheStoreExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ComposableCacheStoreExampleApp.swift
+//  ComposableCacheStoreExample
+//
+//  Created by 0x on 5/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ComposableCacheStoreExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
